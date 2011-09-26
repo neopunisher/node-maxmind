7221519d648a8fc8e244e266038a9d23fa8d1cc8
Carter Cole <carter.cole@gmail.com>
2011-09-26 17:38:46 +0000

    got working

---
+++ main.js
@@ -0,0 +1,20 @@
+var csv = require('csv'),
+    request = require('request'),
+    clr = require('colors'),
+    util = require('util'),
+    url = "http://apathy.cartercole.com/test/maxip.txt",
+    key = "";
+
+//http://geoip3.maxmind.com/f?l=xxxxxxx&i=64.233.183.104
+
+//exports.lookup = function (ip,cb){
+function geo(ip,cb){
+request({uri: url}, function (error, response, body){
+csv().from(body,{columns: ['country', 'region', 'city', 'postal', 'lat', 'lon', 'metro_code', 'area_code', 'isp', 'org', 'err']})
+.on('data',function(data,index){
+cb(data);
+});
+});
+}
+
+geo(null,function(res){console.log(res)});
